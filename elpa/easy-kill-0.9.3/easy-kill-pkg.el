07ac5d22fa38e1def3027f9f065abb4b2f716eac
nohara <selaphymsigoto@gmail.com>
2020-12-01 14:23:53 +0900

    initial commit

---
+++ elpa/easy-kill-0.9.3/easy-kill-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "easy-kill" "0.9.3" "kill & mark things easily" '((emacs "24") (cl-lib "0.5")) :commit "e3b2442e2096cefff94ea8656e49af07fee58f47" :keywords '("killing" "convenience") :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com") :url "https://github.com/leoliu/easy-kill")
