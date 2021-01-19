8b48081c4f3e0cb92ec59413265104dde3f3d005
nohara <selaphymsigoto@gmail.com>
2021-01-20 00:41:55 +0900

    (add mode) add swift-mode, irony-mode

---
+++ elpa/swift-mode-8.1.1/swift-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "swift-mode" "8.1.1" "Major-mode for Apple's Swift programming language."
+  '((emacs "24.4")
+    (seq "2.3"))
+  :commit "e65a80a659c74d0a62b00dff183a0f7fc8385ce1" :keywords
+  ("languages" "swift")
+  :url "https://github.com/swift-emacs/swift-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
